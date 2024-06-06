a00e615d4fa5809110b210ef4b09b699005952a5
Pushkar Wani <pushkarwani2015@gmail.com>
2024-06-06 20:23:18 +0530

    aboutme completed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,87 +1,86 @@
 import React from 'react';
-import author from '../image/me.png'
+import author from '../image/me.png';
 import SectionTitle from './vanilla/SectionTitle';
 
 const AboutMe = () => {
   return (
-    <div id="about" className="container py-5">
+    <div id="about" className="container-fluid py-5 px-5">
       <section id="about">
         <div>
           <SectionTitle title="About" />
-          <div className="flex w-full gap-10 items-center sm:flex-col">
-            <div className="about-text flex flex-col gap-5" id="outer-div">
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                Hi, I'm{" "}
-                <span className="text-highlight-main font-mono">
-                  Pushkar Pramod Wani,
-                  a tech enthusiast
-                </span>
-                {" "}I create versatile software solutions that combine cutting-edge technology with practical functionality.
-              </p>
+          <div className="row w-100 align-items-center lead">
+            <div className="col-lg-7 col-md-12 mb-4">
+              <div className="about-text" id="outer-div">
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  Hi, I'm{" "}
+                  <span className="text-primary font-mono">
+                    Pushkar Pramod Wani, a tech enthusiast
+                  </span>{" "}
+                  I create versatile software solutions that combine cutting-edge technology with practical functionality.
+                </p>
 
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                I have a deep passion for Data Science and Software Development, always eager to explore new possibilities in these fields. I believe that a great data scientist is also a fantastic software developer, capable of solving complex problems and engineering innovative solutions.
-              </p>
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  I have a deep passion for Data Science and Software Development, always eager to explore new possibilities in these fields. I believe that a great data scientist is also a fantastic software developer, capable of solving complex problems and engineering innovative solutions.
+                </p>
 
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                I am currently pursuing a Master's degree in Computer Science at{" "}
-                <a href="https://www.binghamton.edu/">
-                  <span className="text-highlight font-mono">
-                    Binghamton University, NY
-                  </span>
-                </a>{" "}
-                with a cumulative GPA of 3.76/4.00.
-              </p>
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                I'm from{" "}
-                <a href="https://www.google.com/maps/place/Bangalore,+Karnataka,+India/@12.9539975,77.6309395,11z/data=!3m1!4b1!4m6!3m5!1s0x3bae1670cba3ed5d:0x4b8d58bcbfa372b6!8m2!3d12.9715987!4d77.5945627!16zL20vMDMwczA?entry=ttu">
-                  <span className="text-highlight font-mono">Bangalore, India,</span>
-                </a>{" "}
-                where I earned my Bachelor's degree in Electronics and Communication from{" "}
-                <a href="https://pes.edu/">
-                  <span className="text-highlight font-mono">
-                    PES Institute of Technology
-                  </span>
-                </a>{" "}
-                with a cumulative GPA of 7.72/10.
-              </p>
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                I have extensive experience in software development and data engineering. I worked as a Data Engineer at KrtrimaIQ Cognitive Solutions, and as an Implementation Consultant at Alvaria INC.
-              </p>
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                My technical skills include Java, Python, C++, JavaScript, React.JS, Next.JS, MATLAB, Power BI, Tableau, GoodData BI, Azure Data Factory, PostgreSQL, Azure SQL Database, and Microsoft SQL Server.
-              </p>
-              <p className="text-fourth font-mono">
-                <i className="ri-play-line"></i>
-                I also served as the Publicity Head for the IEEE Solid States and Photonics Society during my undergraduate studies at PES Institute of Technology, where I developed and executed marketing strategies and organized events.
-              </p>
-
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  I am currently pursuing a Master's degree in Computer Science at{" "}
+                  <a href="https://www.binghamton.edu/">
+                    <span className="text-primary font-mono">
+                      Binghamton University, NY
+                    </span>
+                  </a>{" "}
+                  with a cumulative GPA of 3.76/4.00.
+                </p>
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  I'm from{" "}
+                  <a href="https://www.google.com/maps/place/Bangalore,+Karnataka,+India/@12.9539975,77.6309395,11z/data=!3m1!4b1!4m6!3m5!1s0x3bae1670cba3ed5d:0x4b8d58bcbfa372b6!8m2!3d12.9715987!4d77.5945627!16zL20vMDMwczA?entry=ttu">
+                    <span className="text-primary font-mono">Bangalore, India,</span>
+                  </a>{" "}
+                  where I earned my Bachelor's degree in Electronics and Communication from{" "}
+                  <a href="https://pes.edu/">
+                    <span className="text-primary font-mono">
+                      PES Institute of Technology
+                    </span>
+                  </a>{" "}
+                  with a cumulative GPA of 7.72/10.
+                </p>
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  I have extensive experience in software development and data engineering. I worked as a Data Engineer at KrtrimaIQ Cognitive Solutions, and as an Implementation Consultant at Alvaria INC.
+                </p>
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  My technical skills include Java, Python, C++, JavaScript, React.JS, Next.JS, MATLAB, Power BI, Tableau, GoodData BI, Azure Data Factory, PostgreSQL, Azure SQL Database, and Microsoft SQL Server.
+                </p>
+                <p className="text-muted font-mono">
+                  <i className="ri-play-line"></i>
+                  I also served as the Publicity Head for the IEEE Solid States and Photonics Society during my undergraduate studies at PES Institute of Technology, where I developed and executed marketing strategies and organized events.
+                </p>
+              </div>
             </div>
-            <div className="about-me rounded-full border border-fourth">
+            <div className="col-lg-5 col-md-12 text-center">
               <img
                 src={author}
                 alt="Pushkar's Portrait"
-                className="rounded-full"
+                className="border border-muted rounded-circle"
                 style={{
-                  minWidth: "100px",
-                  minHeight: "100px",
-                  maxWidth: "300px",
-                  maxHeight: "300px",
+                  minWidth: "300px",
+                  minHeight: "300px",
+                  maxWidth: "400px",
+                  maxHeight: "400px",
                 }}
               />
             </div>
           </div>
         </div>
       </section>
-
     </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,75 +1,156 @@
-import React from 'react'
+import React from 'react';
 import DevIcon from "devicon-react-svg";
 
 const Services = () => {
   return (
     <div id="services" className="services">
       <h1 className="py-5">Some of the technologies I use</h1>
-        <div className="container">
-          <div className="row">
+      <div className="container">
 
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="react"/></div>
-                <h3>React</h3>
-              </div>
+        {/* Programming Languages */}
+        <h2>Programming Languages</h2>
+        <div className="row">
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="java"/></div>
+              <h3>Java</h3>
             </div>
+          </div>
 
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="azure"/></div>
-                <h3>Azure</h3>
-              </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="python"/></div>
+              <h3>Python</h3>
             </div>
+          </div>
 
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="javascript"/></div>
-                <h3>JavaScript</h3>
-              </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="cplusplus"/></div>
+              <h3>C++</h3>
             </div>
+          </div>
 
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="css3"/></div>
-                <h3>CSS3</h3>
-              </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="javascript"/></div>
+              <h3>JavaScript</h3>
             </div>
+          </div>
 
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="python"/></div>
-                <h3>Python</h3>
-              </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="matlab"/></div>
+              <h3>MATLAB</h3>
             </div>
-
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="postgresql"/></div>
-                <h3>PostgreSQL</h3>
-              </div>
-            </div>
-
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon icon="bootstrap"/></div>
-                <h3>Bootstrap</h3>
-              </div>
-            </div>
-
-            <div className="col-md-3 col-sm-6">
-              <div className="box">
-                <div className="circle"><DevIcon className="icon" icon="streamline"/></div>
-                <h3>SQL</h3>
-              </div>
-            </div>
-
-
           </div>
         </div>
-            <h5 className="moreP">and more...</h5>
+
+        {/* Web Development */}
+        <h2>Web Development</h2>
+        <div className="row">
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="html5"/></div>
+              <h3>HTML5</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="css3"/></div>
+              <h3>CSS3</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="react"/></div>
+              <h3>React</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="nextjs"/></div>
+              <h3>Next.JS</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="bootstrap"/></div>
+              <h3>Bootstrap</h3>
+            </div>
+          </div>
+        </div>
+
+        {/* BI/ETL Tools */}
+        <h2>Business Intelligence & ETL Tools</h2>
+        <div className="row">
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="powerbi"/></div>
+              <h3>Power BI</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="tableau"/></div>
+              <h3>Tableau</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="gooddata"/></div>
+              <h3>GoodData BI</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="azure"/></div>
+              <h3>Azure Data Factory</h3>
+            </div>
+          </div>
+        </div>
+
+        {/* Databases */}
+        <h2>Databases</h2>
+        <div className="row">
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="postgresql"/></div>
+              <h3>PostgreSQL</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="microsoftsqlserver"/></div>
+              <h3>Microsoft SQL Server</h3>
+            </div>
+          </div>
+
+          <div className="col-md-3 col-sm-6">
+            <div className="box">
+              <div className="circle"><DevIcon className="icon" icon="azure"/></div>
+              <h3>Azure SQL Database</h3>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <h5 className="moreP">and more...</h5>
     </div>
   )
 }
 
-export default Services
+export default Services;
