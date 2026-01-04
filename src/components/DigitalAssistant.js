bf3872112ce0ae99ccec4dbdc5d10e44fcafdfe4
Pushkar <61586999+2015pushkar@users.noreply.github.com>
2026-01-04 15:05:28 -0600

    removed digital assistant

--- src/components/DigitalAssistant.js
+++ src/components/DigitalAssistant.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { CopilotKit } from "@copilotkit/react-core";
-import { CopilotSidebar } from "@copilotkit/react-ui";
-import "@copilotkit/react-ui/styles.css";
+// Chat generation is disabled in this portfolio.
+// import { CopilotKit } from "@copilotkit/react-core";
+// import { CopilotSidebar } from "@copilotkit/react-ui";
+// import "@copilotkit/react-ui/styles.css";
 
 const DigitalAssistant = () => {
   return (
-    <div>
-    <CopilotKit publicApiKey="AIzaSyDPRXZ94gXnpCMwsKb2Cz8eecRHDYA-7Iw">
-      <CopilotSidebar>
-        Hello
-      </CopilotSidebar>
-    </CopilotKit>
+    null
+    // <div>
+    //   <CopilotKit publicApiKey="REDACTED">
+    //     <CopilotSidebar>
+    //       Hello
+    //     </CopilotSidebar>
+    //   </CopilotKit>
+    // </div>
   );
-    </div>
-  )
 }
 
 export default DigitalAssistant
